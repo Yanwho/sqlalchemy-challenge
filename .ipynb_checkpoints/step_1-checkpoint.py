70af81d6964ea608236fc46011f144ebfbca2fbb
yanwho <yanwho@yahoo.com>
2020-11-03 22:54:01 +0000

    just pushing checkpoints and committing deleted step_1 text file that i changed to .py

---
+++ .ipynb_checkpoints/step_1-checkpoint.py
@@ -0,0 +1,55 @@
+## import required modules
+# matplotlib inline
+from matplotlib import style
+style.use('fivethirtyeight')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import datetime as dt
+from datetime import datetime, timedelta
+from pprint import pprint
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy import inspect
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.types import Date
+
+## Begin Code
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+# Create our session (link) from Python to the DB
+session = Session(bind=engine)
+session
+# reflect an existing database into a new model
+base = automap_base()
+# reflect the tables
+base.prepare(engine, reflect = True)
+# We can view all of the classes that automap found
+base.classes.keys()
+# Save references to each table
+measurement = base.classes.measurement
+station = base.classes.station
+
+# Precipitation Analysis
+# Design a query to retrieve the last 12 months of precipitation data.
+another_last_date = session.query(func.max(measurement.date)).first()[0]
+pprint(another_last_date)
+import datetime
+date_time_obj = datetime.datetime.strptime(another_last_date, '%Y-%m-%d')
+## pprint(date_time_obj)
+one_year_ago2 = date_time_obj - timedelta(days=365)
+## pprint(one_year_ago2)
+results = session.query(measurement.date, measurement.prcp).filter(measurement.date>=one_year_ago2).all()
+print(results)
+# Select only the date and prcp values.
+
+# Load the query results into a Pandas DataFrame and set the index to the date column.
+
+# Sort the DataFrame values by date.
+
+# Plot the results using the DataFrame plot method.
+
+# Use Pandas to print the summary statistics for the precipitation data.
