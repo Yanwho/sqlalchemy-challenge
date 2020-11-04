7cde2dc0a184085697f63930c3c1b770ff662d4b
yanwho <yanwho@yahoo.com>
2020-11-04 00:29:24 +0000

    checkpoints

--- .ipynb_checkpoints/climate_starter-checkpoint.py
+++ .ipynb_checkpoints/climate_starter-checkpoint.py
@@ -4,7 +4,7 @@
 # In[1]:
 
 
-get_ipython().run_line_magic('matplotlib', 'inline')
+# get_ipython().run_line_magic('matplotlib', 'inline')
 from matplotlib import style
 style.use('fivethirtyeight')
 import matplotlib.pyplot as plt
@@ -70,7 +70,6 @@ base.classes.keys()
 # Save references to each table
 measurement = base.classes.measurement
 station = base.classes.station
-station
 
 
 # In[9]:
@@ -110,7 +109,7 @@ print(pd.read_sql(cmd, con=engine))
 
 
 last_date = session.query(func.max(measurement.date)).first()[0]
-last_date
+print("The last day data was collected was: " + last_date)
 
 
 # In[12]:
@@ -143,7 +142,7 @@ type(dt.timedelta(days=365))
 
 
 one_year_ago = date_time_obj - timedelta(days=365)
-print(one_year_ago)
+print("The last year of data collection began: " + str(one_year_ago))
 
 
 # In[17]:
@@ -157,6 +156,7 @@ one_year_ago_query
 
 
 last_year_df = pd.read_sql(one_year_ago_query.statement, one_year_ago_query.session.bind)
+print("This dataframe includes the last year of data")
 last_year_df
 
 
@@ -171,8 +171,9 @@ print(prcp_data)
 
 
 # Use Pandas to calcualte the summary statistics for the precipitation data
-prcp_data[["prcp"]].describe()
-
+prcp_summary = prcp_data[["prcp"]].describe()
+print("The summary of precip data from the last year is: ") 
+prcp_summary
 
 # In[21]:
 
@@ -219,92 +220,92 @@ print(activity_count_2)
 
 # This function called `calc_temps` will accept start date and end date in the format '%Y-%m-%d' 
 # and return the minimum, average, and maximum temperatures for that range of dates
-def calc_temps(start_date, end_date):
-    """TMIN, TAVG, and TMAX for a list of dates.
+# def calc_temps(start_date, end_date):
+#     """TMIN, TAVG, and TMAX for a list of dates.
     
-    Args:
-        start_date (string): A date string in the format %Y-%m-%d
-        end_date (string): A date string in the format %Y-%m-%d
+#     Args:
+#         start_date (string): A date string in the format %Y-%m-%d
+#         end_date (string): A date string in the format %Y-%m-%d
         
-    Returns:
-        TMIN, TAVE, and TMAX
-    """
+#     Returns:
+#         TMIN, TAVE, and TMAX
+#     """
     
-    return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+#     return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
 
-# function usage example
-print(calc_temps('2012-02-28', '2012-03-05'))
-
-
-# In[ ]:
+# # function usage example
+# print(calc_temps('2012-02-28', '2012-03-05'))
 
 
-# Use your previous function `calc_temps` to calculate the tmin, tavg, and tmax 
-# for your trip using the previous year's data for those same dates.
+# # In[ ]:
 
 
-# In[ ]:
+# # Use your previous function `calc_temps` to calculate the tmin, tavg, and tmax 
+# # for your trip using the previous year's data for those same dates.
 
 
-# Plot the results from your previous query as a bar chart. 
-# Use "Trip Avg Temp" as your Title
-# Use the average temperature for the y value
-# Use the peak-to-peak (tmax-tmin) value as the y error bar (yerr)
+# # In[ ]:
 
 
-# In[ ]:
+# # Plot the results from your previous query as a bar chart. 
+# # Use "Trip Avg Temp" as your Title
+# # Use the average temperature for the y value
+# # Use the peak-to-peak (tmax-tmin) value as the y error bar (yerr)
 
 
-# Calculate the total amount of rainfall per weather station for your trip dates using the previous year's matching dates.
-# Sort this in descending order by precipitation amount and list the station, name, latitude, longitude, and elevation
+# # In[ ]:
 
 
-# In[ ]:
+# # Calculate the total amount of rainfall per weather station for your trip dates using the previous year's matching dates.
+# # Sort this in descending order by precipitation amount and list the station, name, latitude, longitude, and elevation
 
 
-# Create a query that will calculate the daily normals 
-# (i.e. the averages for tmin, tmax, and tavg for all historic data matching a specific month and day)
+# # In[ ]:
 
-def daily_normals(date):
-    """Daily Normals.
+
+# # Create a query that will calculate the daily normals 
+# # (i.e. the averages for tmin, tmax, and tavg for all historic data matching a specific month and day)
+
+# def daily_normals(date):
+#     """Daily Normals.
     
-    Args:
-        date (str): A date string in the format '%m-%d'
+#     Args:
+#         date (str): A date string in the format '%m-%d'
         
-    Returns:
-        A list of tuples containing the daily normals, tmin, tavg, and tmax
+#     Returns:
+#         A list of tuples containing the daily normals, tmin, tavg, and tmax
     
-    """
+#     """
     
-    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
-    return session.query(*sel).filter(func.strftime("%m-%d", Measurement.date) == date).all()
+#     sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+#     return session.query(*sel).filter(func.strftime("%m-%d", Measurement.date) == date).all()
     
-daily_normals("01-01")
+# daily_normals("01-01")
 
 
-# In[ ]:
+# # In[ ]:
 
 
-# calculate the daily normals for your trip
-# push each tuple of calculations into a list called `normals`
+# # calculate the daily normals for your trip
+# # push each tuple of calculations into a list called `normals`
 
-# Set the start and end date of the trip
+# # Set the start and end date of the trip
 
-# Use the start and end date to create a range of dates
+# # Use the start and end date to create a range of dates
 
-# Stip off the year and save a list of %m-%d strings
+# # Stip off the year and save a list of %m-%d strings
 
-# Loop through the list of %m-%d strings and calculate the normals for each date
-
-
-# In[ ]:
+# # Loop through the list of %m-%d strings and calculate the normals for each date
 
 
-# Load the previous query results into a Pandas DataFrame and add the `trip_dates` range as the `date` index
+# # In[ ]:
 
 
-# In[ ]:
+# # Load the previous query results into a Pandas DataFrame and add the `trip_dates` range as the `date` index
 
 
-# Plot the daily normals as an area plot with `stacked=False`
+# # In[ ]:
+
+
+# # Plot the daily normals as an area plot with `stacked=False`
 
