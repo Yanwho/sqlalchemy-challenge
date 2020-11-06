a56660f597aa21659f0e8d543b39c00e62cd7a74
yanwho <yanwho@yahoo.com>
2020-11-06 20:37:06 +0000

    began flask portion

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -367,6 +367,17 @@ daily_normals("01-01")
 
 # In[ ]:
 
+## Flask API
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+import os
+app = Flask(__name__)
 
+@app.route("/")
+def main():
+	return "Thesea are the available routes"
 
+if __name__ == "__main__":
+	app.run(debug=True)
 
